93b23de008641c9e00c20ad3ffc375e348c9935c
Marcoooooooooooo <b09901089@g.ntu.edu.tw>
2024-04-02 21:40:24 +0800

    first commit

---
+++ f04943000_pa1/src/partitioner.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+#include <vector>
+#include <cmath>
+#include <map>
+#include "cell.h"
+#include "net.h"
+#include "partitioner.h"
+using namespace std;
+
+
+void Partitioner::parseInput(fstream& inFile)
+{
+    string str;
+    // Set balance factor
+    inFile >> str;
+    _bFactor = stod(str);
+
+    // Set up whole circuit
+    while (inFile >> str) {
+        if (str == "NET") {
+            string netName, cellName, tmpCellName = "";
+            inFile >> netName;
+            int netId = _netNum;
+            _netArray.push_back(new Net(netName));
+            _netName2Id[netName] = netId;
+            while (inFile >> cellName) {
+                if (cellName == ";") {
+                    tmpCellName = "";
+                    break;
+                }
+                else {
+                    // a newly seen cell
+                    if (_cellName2Id.count(cellName) == 0) {
+                        int cellId = _cellNum;
+                        _cellArray.push_back(new Cell(cellName, 0, cellId));
+                        _cellName2Id[cellName] = cellId;
+                        _cellArray[cellId]->addNet(netId);
+                        _cellArray[cellId]->incPinNum();
+                        _netArray[netId]->addCell(cellId);
+                        ++_cellNum;
+                        tmpCellName = cellName;
+                    }
+                    // an existed cell
+                    else {
+                        if (cellName != tmpCellName) {
+                            assert(_cellName2Id.count(cellName) == 1);
+                            int cellId = _cellName2Id[cellName];
+                            _cellArray[cellId]->addNet(netId);
+                            _cellArray[cellId]->incPinNum();
+                            _netArray[netId]->addCell(cellId);
+                            tmpCellName = cellName;
+                        }
+                    }
+                }
+            }
+            ++_netNum;
+        }
+    }
+    return;
+}
+
+void Partitioner::partition()
+{
+
+}
+
+void Partitioner::printSummary() const
+{
+    cout << endl;
+    cout << "==================== Summary ====================" << endl;
+    cout << " Cutsize: " << _cutSize << endl;
+    cout << " Total cell number: " << _cellNum << endl;
+    cout << " Total net number:  " << _netNum << endl;
+    cout << " Cell Number of partition A: " << _partSize[0] << endl;
+    cout << " Cell Number of partition B: " << _partSize[1] << endl;
+    cout << "=================================================" << endl;
+    cout << endl;
+    return;
+}
+
+void Partitioner::reportNet() const
+{
+    cout << "Number of nets: " << _netNum << endl;
+    for (size_t i = 0, end_i = _netArray.size(); i < end_i; ++i) {
+        cout << setw(8) << _netArray[i]->getName() << ": ";
+        vector<int> cellList = _netArray[i]->getCellList();
+        for (size_t j = 0, end_j = cellList.size(); j < end_j; ++j) {
+            cout << setw(8) << _cellArray[cellList[j]]->getName() << " ";
+        }
+        cout << endl;
+    }
+    return;
+}
+
+void Partitioner::reportCell() const
+{
+    cout << "Number of cells: " << _cellNum << endl;
+    for (size_t i = 0, end_i = _cellArray.size(); i < end_i; ++i) {
+        cout << setw(8) << _cellArray[i]->getName() << ": ";
+        vector<int> netList = _cellArray[i]->getNetList();
+        for (size_t j = 0, end_j = netList.size(); j < end_j; ++j) {
+            cout << setw(8) << _netArray[netList[j]]->getName() << " ";
+        }
+        cout << endl;
+    }
+    return;
+}
+
+void Partitioner::writeResult(fstream& outFile)
+{
+    stringstream buff;
+    buff << _cutSize;
+    outFile << "Cutsize = " << buff.str() << '\n';
+    buff.str("");
+    buff << _partSize[0];
+    outFile << "G1 " << buff.str() << '\n';
+    for (size_t i = 0, end = _cellArray.size(); i < end; ++i) {
+        if (_cellArray[i]->getPart() == 0) {
+            outFile << _cellArray[i]->getName() << " ";
+        }
+    }
+    outFile << ";\n";
+    buff.str("");
+    buff << _partSize[1];
+    outFile << "G2 " << buff.str() << '\n';
+    for (size_t i = 0, end = _cellArray.size(); i < end; ++i) {
+        if (_cellArray[i]->getPart() == 1) {
+            outFile << _cellArray[i]->getName() << " ";
+        }
+    }
+    outFile << ";\n";
+    return;
+}
+
+void Partitioner::clear()
+{
+    for (size_t i = 0, end = _cellArray.size(); i < end; ++i) {
+        delete _cellArray[i];
+    }
+    for (size_t i = 0, end = _netArray.size(); i < end; ++i) {
+        delete _netArray[i];
+    }
+    return;
+}

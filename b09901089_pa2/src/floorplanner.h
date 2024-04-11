50d5f2eb0d27ed8865e278b536fa64679b72db94
Marc0620 <b09901089@ntu.edu.tw>
2024-04-11 13:50:51 +0800

    pa2

---
+++ b09901089_pa2/src/floorplanner.h
@@ -0,0 +1,34 @@
+
+#ifndef FLOOR_PLANNER_H
+#define FLOOR_PLANNER_H
+
+#include <vector>
+#include <string>
+#include "module.h"
+class floorplanner
+{
+private:
+  /* data */
+  vector<Net> _nets;
+  vector<
+public:
+  floorplanner(/* args */);
+  ~floorplanner();
+};
+
+floorplanner::floorplanner(/* args */)
+{
+}
+
+floorplanner::~floorplanner()
+{
+}
+
+
+
+
+
+
+
+
+#endif
